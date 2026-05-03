6b0d4312092d7acabbf2d371b8dd91f36c4a4ffd
Tosccony <kody.verhulp@gmail.com>
2026-05-03 14:39:13 -0300

    Add writers registry with shared WriteProject interface

---
+++ writers/index.ts
@@ -0,0 +1,27 @@
+import * as nextjs from './nextjs';
+import type { DesignDNA } from '../presets';
+
+export type Framework = 'nextjs';
+
+export interface WriteProjectOptions {
+  dna: DesignDNA;
+  outDir: string;
+  overwrite?: boolean;
+}
+
+export interface WriteProjectResult {
+  outDir: string;
+  filesWritten: string[];
+}
+
+export type WriteProject = (opts: WriteProjectOptions) => WriteProjectResult;
+
+export const writers: Record<Framework, WriteProject> = {
+  nextjs: nextjs.writeProject,
+};
+
+export const availableFrameworks: Framework[] = Object.keys(writers) as Framework[];
+
+export function isFramework(value: string): value is Framework {
+  return value in writers;
+}
